e17970289d86e492c9e0f1e8378a8787f84bb7a4
Backup158 <beekoop158+gh@gmail.com>
2025-01-01 02:32:01 -0600

    Updated OBS sinks for Mint 22
    looks like this version of pactl can't use spaces in names

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/bash
 echo "G-good Meowning :3"
 
-# Creates separate audio sinks to separate tracks for OBS
-#   So I can avoid recording background music while making gaming clips or tutorials
-pactl load-module module-null-sink sink_name=OBSonly
-pactl load-module module-combine-sink slaves=OBSonly,alsa_output.pci-0000_0b_00.4.analog-stereo sink_name=OBSandHeadphones
+# OBS audio sinks
+#   Sink name is for internal use. Cannot use spaces
+#   If there is a device description, that will be used as the display name (when human is reading)
+pactl load-module module-null-sink sink_name="OBS" sink_properties="'device.description=\"OBS Only\"'"
+pactl load-module module-combine-sink sink_name="OBS+Headphones" sink_properties="'device.description=\"Combined (OBS/Headphones)\"'" slaves="OBS",alsa_output.pci-0000_0b_00.4.analog-stereo
+# Rename headphone sink for human readibility
+pactl load-module update-sink-proplist alsa_output.pci-0000_0b_00.4.analog-stereo device.description="Headphones"
